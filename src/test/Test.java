6f0f788174a134f3098cf4ebfd25f1e46b788326
RoniGu <RoniGu@DESKTOP-LJPG4PA>
2017-12-18 23:08:06 +0200

    Test

---
+++ src/test/Test.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * AllTests Testing all the Testing Classes.
+ */
+
+	@RunWith(Suite.class)
+	@SuiteClasses({ Point3DTest.class, DateTest.class, WifiTest.class })
+	
+	public class Test {
+		
+		
+	}
+
+//}
