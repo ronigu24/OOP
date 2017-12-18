1ee24235887ac8a918f7c026010e705fb9b9fe1e
RoniGu <RoniGu@DESKTOP-LJPG4PA>
2017-12-18 23:10:17 +0200

    Point 3D - lat, lon, alt

---
+++ src/hw0/Point3D.java
@@ -0,0 +1,50 @@
+package hw0;
+
+/**
+* The Point2D class defines a point representing a location in (lat,lon,alt) coordinate space.
+*/
+
+public class Point3D {
+
+	private double Lat;
+	private double Lon;
+	private double Alt;
+	
+	public Point3D(double lat,double lon,double alt)
+	{
+		this.Lat=lat;
+		this.Lon=lon;
+		this.Alt=alt;
+	}
+
+	public double getLat() {
+		return Lat;
+	}
+
+	public void setLat(double lat) {
+		Lat = lat;
+	}
+
+	public double getLon() {
+		return Lon;
+	}
+
+	public void setLon(double lon) {
+		Lon = lon;
+	}
+
+	public double getAlt() {
+		return Alt;
+	}
+
+	public void setAlt(double alt) {
+		Alt = alt;
+	}
+
+	public boolean compare(Point3D p2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+
+}
